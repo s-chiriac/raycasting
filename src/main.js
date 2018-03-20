7bdb0ac72ea35aa6d90ebdfdd1c8372a9e89ac59
Sorin Chiriac <chirias@maclon0155.igi.ig.local>
2018-03-20 14:54:27 +0000

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/**
+ * A two dimensional array describing the level
+ * 0's are empty spaces, 1's are walls
+ *
+ * @type Object
+ */
 const LEVEL_1_MAP = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -25,27 +31,47 @@ const LEVEL_1_MAP = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
+/**
+ * The resolution of the game
+ *
+ * @type {{WIDTH: number, HEIGHT: number}}
+ */
 const RESOLUTION = {
   WIDTH: 160,
   HEIGHT: 90
 };
 
+// Canvas and canvas context
 let canvas;
 let context;
+
+// The size of a canvas pixel
 let pixelSize;
+
+// Game state
 let gameActive = false;
 
+// Player position
 let posX, posY;
+
+// Player direction
 let dirX, dirY;
+
+// Camera plane
 let planeX, planeY;
 
+// Used to work out FPS and movement/rotation amounts
 let time, oldTime, frameTime;
+
+// Speed of movement and rotation
 let moveSpeed, rotSpeed;
 
+// Describe which player movement directions are active
 let left = false;
 let right = false;
 let up = false;
 let down = false;
+
 
 function game() {
   canvas = document.getElementById('canvas');
@@ -69,34 +95,49 @@ function startGame() {
 
   gameActive = true;
 
+  // Start game
   gameLoop();
 }
 
 function gameLoop() {
+  // Fill top half with a black rectangle
   context.fillStyle = 'rgb(0, 0, 0)';
   context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight / 2);
 
+  // Fill bottom half with dark grey rectangle
   context.fillStyle = 'rgb(48, 48, 48)';
   context.fillRect(0, canvas.clientHeight / 2, canvas.clientWidth, canvas.clientHeight / 2);
 
+  // Loop through every vertical line on the canvas
   for (let x = 0; x < RESOLUTION.WIDTH; x++) {
+
+    // Find the position of the ray we are checking on the camera plane, from -1 to 1
     let cameraX = 2 * x / RESOLUTION.WIDTH - 1;
+
+    // Calculate ray vector
     let rayDirX = dirX + planeX * cameraX;
     let rayDirY = dirY + planeY * cameraX;
 
+    // Save the square on the map the player is in
     let mapX = Math.floor(posX);
     let mapY = Math.floor(posY);
 
     let sideDistX, sideDistY;
 
+    // Distances required for ray to travel from one map square to the next
     let deltaDistX = Math.sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
     let deltaDistY = Math.sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
 
+    // Used later to calculate "real" distance to the wall to prevent fisheye effect
     let perpWallDist;
 
+    // General direction the ray is travelling in
     let stepX, stepY;
 
+    // 1 if a wall is hit, 0 otherwise
     let hit = 0;
+
+    // Which side of the wall has been hit (for texturing purposes)
     let side;
 
     if (rayDirX < 0) {
@@ -219,44 +260,45 @@ function gameLoop() {
   }
 }
 
-document.addEventListener('keydown', (event) => {
-  switch (event.keyCode) {
-    case 37:
-      left = true;
-      break;
-    case 39:
-      right = true;
-      break;
-    case 38:
-      up = true;
-      break;
-    case 40:
-      down = true;
-      break;
-  }
+function updateDirection(event, direction, value) {
+  direction = value;
 
   event.preventDefault();
   event.stopPropagation();
+}
+
+document.addEventListener('keydown', (event) => {
+  switch (event.keyCode) {
+    case 37:
+      updateDirection(event, left, true);
+      break;
+    case 39:
+      updateDirection(event, right, true);
+      break;
+    case 38:
+      updateDirection(event, up, true);
+      break;
+    case 40:
+      updateDirection(event, down, true);
+      break;
+  }
 });
 
 document.addEventListener('keyup', (event) => {
   switch (event.keyCode) {
     case 37:
-      left = false;
+      updateDirection(event, left, false);
       break;
     case 39:
-      right = false;
+      updateDirection(event, right, false);
       break;
     case 38:
-      up = false;
+      updateDirection(event, up, false);
       break;
     case 40:
-      down = false;
+      updateDirection(event, down, false);
       break;
   }
-
-  event.preventDefault();
-  event.stopPropagation();
 });
 
 game();
